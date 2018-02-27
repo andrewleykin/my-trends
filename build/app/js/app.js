ff84bdf486a4617355a650705ad7b8325477eba8
AndrewLeykin <katashi1328@mail.ru>
2018-02-27 19:22:49 +0300

    start project

--- build/app/js/app.js
+++ build/app/js/app.js
@@ -1,8 +1,40 @@
 // Начальная функция
 
+// появление описания
 (function(){
-	console.log('app');
+	var descr = $('.services__el'),
+		descrActive = 'services__el--active'
+	$('.services__image').each(function(index, el) {
+		$(el).mouseover(function() {
+			descr.eq(index).addClass(descrActive);
+		});
+		$(el).mouseleave(function() {
+			descr.eq(index).removeClass(descrActive);
+		});
+	});
+})();
+
+// фиксация попапа
+(function(){
+	function scrollForm(){
+		var preparation = $('.preparation')
+		var form = preparation.find('.preparation__form-wrap');
+		var windowScroll = $(window).scrollTop();
+
+		if ((windowScroll >= 3576) && (windowScroll <  4208)) {
+			var top = windowScroll - 3688;
+			$(form).css('top', top);
+		}
+	}
+	$(window).on('scroll', function() {
+		scrollForm();
+	});
 })();
 $(document).ready(function () {
     svg4everybody({});
 });
+// Библиотека wow.js для анимации
+
+(function () {
+	new WOW().init();
+})();
